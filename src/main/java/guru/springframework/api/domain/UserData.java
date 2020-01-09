28971dc777b688383f0eb862d86687239e5ccd5e
Pavlo Kolesnichenko <pavlo.kolesnichenko@gmail.com>
2020-01-09 17:59:34 +0200

    added RestTemplate

---
+++ src/main/java/guru/springframework/api/domain/UserData.java
@@ -0,0 +1,16 @@
+package guru.springframework.api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserData {
+    private List<User> data;
+}
